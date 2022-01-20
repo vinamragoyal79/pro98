affb0f6fc91b4e96b141b6a9796c1106b288afd1
vinamragoyal79 <85055812+vinamragoyal79@users.noreply.github.com>
2022-01-20 17:59:25 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+def swapText():
+    file1 = input("Enter file name = ")
+    file2 = input("Enter file name = ")
+
+    with open(file1, 'r') as a:
+        data_a = a.read()
+    with open(file2, 'r') as b:
+        data_b = b.read()
+    with open(file1, 'w') as a:
+        a.write(data_b)
+    with open(file2, 'w') as b:
+        b.write(data_a)
+
+swapText()
